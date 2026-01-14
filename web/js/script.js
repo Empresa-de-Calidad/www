126569132fd3d4aef81f32cc2de866440301b2fa
Iván <lidoryph@mailbox.org>
2026-01-14 22:39:35 +0100

    que putisima paja acabarlo

--- web/js/script.js
+++ web/js/script.js
@@ -1,7 +1,14 @@
-function openMain() {
-        window.location.replace("main.html");
-}
-
 function openIndex() {
         window.location.replace("index.html");
+}
+
+var navSearchStatus = false;
+
+function nav_search_menu() {
+        if(navSearchStatus == false) navSearchStatus == true;
+        else navSearchStatus == false;
+
+        extension = document.nav_search_menu;
+        
+        
 }
